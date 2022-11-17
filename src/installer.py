1932c024804317a7a05bc623940bf771b84611cc
redshifttt <num@privatevoid.net>
2022-11-17 22:34:44 +0000

    cleanup

--- src/installer.py
+++ src/installer.py
@@ -1,10 +1,13 @@
 import shutil
-import src.parser as parser
-import src.stower as stower
 import os
 import urllib.request
 import mimetypes
 import subprocess
+import tempfile
+
+''' Arachnid Specific '''
+import src.parser as parser
+import src.stower as stower
 import src.log as log
 
 def get_sources(build_file, build_directory):
@@ -14,56 +17,56 @@ def get_sources(build_file, build_directory):
         *_, source_file = source.split("/")
         urllib.request.urlretrieve(source, os.path.join(build_directory, source_file))
 
-def build_install_package(build_file):
+def build_and_install_package(build_file):
     build_file = os.path.abspath(build_file)
     package = parser.parse_build_file(build_file)
 
-    archives_for_removal = []
+    source_directories = []
 
-    build_directory = os.path.join("/tmp", "arachnid", "build")
+    arachnid_build_directory = tempfile.mkdtemp()
 
-    if os.path.exists(build_directory):
-        log.log(f"{build_directory} exists already, removing")
-        shutil.rmtree(build_directory, ignore_errors=False)
+    if os.path.exists(arachnid_build_directory):
+        log.log(f"{arachnid_build_directory} exists already, removing")
+        shutil.rmtree(arachnid_build_directory, ignore_errors=False)
 
-    log.log(f"creating {build_directory}")
+    log.log(f"creating {arachnid_build_directory}")
 
-    os.makedirs(build_directory)
+    os.makedirs(arachnid_build_directory)
 
-    log.log(f"moving into {build_directory}")
-    os.chdir(build_directory)
+    log.log(f"moving into {arachnid_build_directory}")
+    os.chdir(arachnid_build_directory)
 
-    get_sources(build_file, build_directory)
+    get_sources(build_file, arachnid_build_directory)
 
-    log.log(f"extracting archives in {build_directory}")
-    extracted_dir_name = package.meta.name
-    with os.scandir(build_directory) as dir:
+    log.log(f"extracting archives in {arachnid_build_directory}")
+    package_source_directory = package.meta.name
+    with os.scandir(arachnid_build_directory) as dir:
         for entry in dir:
             try:
-                shutil.unpack_archive(entry.name, extracted_dir_name)
-                archives_for_removal.append(entry.path)
+                shutil.unpack_archive(entry.name, package_source_directory)
+                source_directories.append(entry.path)
             except ValueError:
                 continue
 
-    for archive in archives_for_removal:
+    for archive in source_directories:
         log.log(f"deleting {archive}")
         os.remove(archive)
 
-    log.log(f"moving into extracted directory {extracted_dir_name}")
-    os.chdir(extracted_dir_name)
+    log.log(f"moving into extracted directory {package_source_directory}")
+    os.chdir(package_source_directory)
 
-    commands = [command for command in package.build.install]
-    subprocess.run(" && ".join(commands), shell=True)
+    build_commands = [command for command in package.build.install]
+    subprocess.run(" && ".join(build_commands), shell=True)
 
     log.log(f"removing {os.getcwd()}")
     os.chdir("..")
-    shutil.rmtree(extracted_dir_name, ignore_errors=False)
+    shutil.rmtree(package_source_directory, ignore_errors=False)
 
-    with os.scandir("/opt") as opt:
-        for entry in opt:
+    with os.scandir("/opt") as package_installation_directory:
+        for entry in package_installation_directory:
             if f"{package.meta.name}-{package.meta.version}" in entry.name:
-                target_install_directory = f"{entry.path}/"
+                install_directory_within_target = f"{entry.path}/"
 
     stow = stower.Stower()
-    stow.stow(target_install_directory)
+    stow.stow(install_directory_within_target)
 
