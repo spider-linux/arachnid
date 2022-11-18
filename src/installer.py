1d334e63fa380284d72eb9f5bd37cf07e2030e1f
redshifttt <num@privatevoid.net>
2022-11-18 19:10:10 +0000

    very early days database

--- src/installer.py
+++ src/installer.py
@@ -4,6 +4,7 @@ import urllib.request
 import mimetypes
 import subprocess
 import tempfile
+import sqlite3
 
 ''' Arachnid Specific '''
 import src.parser as parser
@@ -18,6 +19,7 @@ def get_sources(build_file, build_directory):
         urllib.request.urlretrieve(source, os.path.join(build_directory, source_file))
 
 def build_and_install_package(build_file):
+    # 1: Prepare the directories
     build_file = os.path.abspath(build_file)
     package = parser.parse_build_file(build_file)
 
@@ -48,12 +50,14 @@ def build_and_install_package(build_file):
     log.log(f"moving into extracted directory {package_source_directory}")
     os.chdir(package_source_directory)
 
+    # 2: Run the commands
     build_commands = [command for command in package.build.install]
     subprocess.run(" && ".join(build_commands), shell=True)
 
+    # 3: Clean-up
     os.chdir(f"{arachnid_build_directory}/..")
 
-    log.log(f"removing {arachnid_build_directory}")
+    log.log(f"removing build directory at {arachnid_build_directory}")
     shutil.rmtree(arachnid_build_directory, ignore_errors=False)
 
     with os.scandir("/opt") as package_installation_directory:
@@ -61,6 +65,12 @@ def build_and_install_package(build_file):
             if f"{package.meta.name}-{package.meta.version}" in entry.name:
                 install_directory_within_target = f"{entry.path}/"
 
+    # 4: Actual install section
     stow = stower.Stower()
     stow.stow(install_directory_within_target)
 
+    conn = sqlite3.connect("/var/db/arachnid.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO packages VALUES(?, ?, ?)", (package.meta.name, package.meta.version, package.meta.description))
+    conn.commit()
+    conn.close()
