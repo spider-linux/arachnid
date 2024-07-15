9358fa6c096c93fae2ac2946c8b8cfc43adefecc
redshifttt <num@privatevoid.net>
2024-07-15 19:39:50 +0100

    arachnid/lua: simple example of potential function

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"log"
+	"os"
 
 	"github.com/Shopify/go-lua"
 	"github.com/spf13/cobra"
@@ -15,9 +16,9 @@ var (
         Use: "arachnid",
         Short: "package/system manager for Spider Linux",
     }
-    cmdBootstrap = &cobra.Command{
-        Use: "bootstrap",
-        Short: "Bootstrap the system",
+    cmdRun = &cobra.Command{
+        Use: "run",
+        Short: "Run script",
         Args: cobra.ExactArgs(1),
         Run: func(cmd *cobra.Command, args []string) {
             // TODO: add check to see if argument is actually a file before running DoFile.
@@ -30,15 +31,36 @@ var (
 )
 
 func createDirStructure(L *lua.State) int {
-    fmt.Println("creating dir structure in ./")
+    if L.IsTable(1) {
+        dirsToCreate := make([]string, 0)
+        tablePos := 1
+        tableLength := L.RawLength(1) + 1
+
+        for i := 1; i <= tableLength; i++ {
+            // Pushes table value (type independant) to stack
+            L.RawGetInt(1, i)
+        }
+
+        for i := tablePos + 1; i <= tableLength; i++ {
+            s, ok := L.ToString(i)
+            if !ok {
+                log.Fatalln("error: not string or number")
+            }
+            dirsToCreate = append(dirsToCreate, s)
+        }
+
+        for _, d := range dirsToCreate {
+            os.Mkdir(d, 0755)
+        }
+    }
     return 0
 }
 
 func init(){
     lua.BaseOpen(L)
-    L.Register("create_dir_structure", createDirStructure)
+    L.Register("create_root_structure", createDirStructure)
 
-    rootCmd.AddCommand(cmdBootstrap)
+    rootCmd.AddCommand(cmdRun)
 }
 
 func main(){
--- test.lua
+++ test.lua
@@ -2,4 +2,20 @@ local meta = {
     name = "test",
     version = "v0.0.0",
 }
-create_dir_structure()
+
+-- Just some for examples
+local dirs_to_make = {
+    "spiderroot",
+    "spiderroot/bin",
+    "spiderroot/etc",
+    "spiderroot/boot",
+    "spiderroot/dev",
+    "spiderroot/sys",
+    "spiderroot/lib",
+    "spiderroot/run",
+    "spiderroot/root",
+    "spiderroot/tmp",
+    "spiderroot/usr",
+}
+
+create_root_structure(dirs_to_make)
