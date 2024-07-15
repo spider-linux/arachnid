f2603ae0369c815b9bb7e7da5c199384e6469b89
redshifttt <num@privatevoid.net>
2024-07-15 23:20:08 +0100

    test.lua: add more dirs

--- test.lua
+++ test.lua
@@ -6,16 +6,20 @@ local meta = {
 -- Just some for examples
 local dirs_to_make = {
     "spiderroot",
-    "spiderroot/bin",
     "spiderroot/etc",
-    "spiderroot/boot",
-    "spiderroot/dev",
-    "spiderroot/sys",
-    "spiderroot/lib",
-    "spiderroot/run",
-    "spiderroot/root",
-    "spiderroot/tmp",
+    "spiderroot/var",
+    "spiderroot/lib64",
     "spiderroot/usr",
+    "spiderroot/usr/bin",
+    "spiderroot/usr/lib",
+    "spiderroot/usr/sbin",
+
+    -- "spiderroot/dev",
+    -- "spiderroot/sys",
+    -- "spiderroot/lib",
+    -- "spiderroot/run",
+    -- "spiderroot/root",
+    -- "spiderroot/tmp",
 }
 
 create_root_structure(dirs_to_make)
